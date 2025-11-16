6051d240ae159b7fe971dc6bc122023b3423b222
Krishna Warfade <krishnawarfade@gmail.com>
2025-11-16 18:41:09 +0530

    add: added delete code for BST.c in Tree/BST.c

--- Tree/BST.c
+++ Tree/BST.c
@@ -73,6 +73,59 @@ Node *Insert(Node *t, int x)
     return q;
 }
 
+int height (Node* root) {
+    int a = 0;
+    int b = 0;
+
+    if (root == 0)
+        return 0;
+    a = height(root->lchild);
+    b = height(root->rchild);
+
+    return (a > b) ? (1 + a) : (1 + b);
+}
+
+Node *inorder_predec (Node *p) { // for largest value in left subtree by going as right as possible
+    while (p && p->rchild)
+        p = p->rchild;
+    return p;
+}
+
+Node *inorder_succ (Node *p) { // receives right subtree of a node and goes as left as possible
+    while (p && p->lchild)
+        p = p->lchild;
+    return p;
+}
+
+Node *delete (int key, Node *t) {
+    if (!t)
+        return NULL;
+    
+    if (key < t->data)
+        t->lchild = delete(key, t->lchild);
+    else if (key > t->data)
+        t->rchild = delete(key, t->rchild);
+    else { // key node found
+        if (!t->lchild && !t->rchild) {
+            free(t);
+            return NULL;
+        }
+        else if (height(t->lchild) > height(t->rchild)) {
+            Node *predecessor = inorder_predec(t->lchild);
+
+            t->data = predecessor->data;
+            t->lchild = delete(predecessor->data, t->lchild);
+        }
+        else {
+            Node *successor = inorder_succ(t->rchild);
+
+            t->data = successor->data;
+            t->rchild = delete(successor->data, t->rchild);
+        }
+    }
+    return t;
+}
+
 void Inorder(Node *p)
 {
     if (p)
@@ -90,8 +143,12 @@ int main()
     Insert(root, 5);
     Insert(root, 7);
     Inorder(root);
+
     Node *t = RSearch(root, 5);
     printf("%d", t->data);
-    
+
+    printf("\n");
+    root = delete(3, root);
+    Inorder(root);
     return 0;
 }
