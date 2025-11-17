fd8d8795bc1caed263f4739fcda6884dbc24a20f
Krishna Warfade <krishnawarfade@gmail.com>
2025-11-17 15:49:57 +0530

    fix: Fixed and implemented part2_c.c code (nice code :) , CLL)

--- part2_2.c
+++ part2_2.c
@@ -1,3 +1,7 @@
+/*
+ - Circular Linked List
+*/
+
 #include<stdio.h>
 
 typedef struct bhai{
@@ -5,22 +9,39 @@ typedef struct bhai{
 	struct bhai *a;
 }bhai;
 
+void printList (bhai *head) {
+	if (head == NULL) {
+		printf("list is empty\n");
+		return;
+	}
+	bhai *temp = head;
+
+	do {
+		printf("%c ", temp->c);
+		temp = temp->a;
+	} while (temp != head);
+
+	return;
+}
+
 int main(){
 	bhai *p, *q, *r;
 	bhai l, m, n;
 	
 	p = &l;
-	p->c = "w";
+	p->c = 'w';
 	p->a = &m;
 	q = p->a;
 	
-	q->c = "x";
+	q->c = 'x';
 	q->a = &n;
 	r = q->a;
 	
-	r->c = "y";
+	r->c = 'y';
 	r->a = p;
 	
+	printList(p);
+
 	q = r = NULL;
-	return;
+	return 0;
 }
