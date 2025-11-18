2e7b698510c8aae28d29b73fead271a96301c1f5
Krishna Warfade <krishnawarfade@gmail.com>
2025-11-18 15:02:26 +0530

    fix: Removed redundant statmt

--- LinkedList/LLDisplay.c
+++ LinkedList/LLDisplay.c
@@ -337,26 +337,22 @@ void merge_LL(struct Node *p,struct Node *q){
     if(p->data<q->data){
         third=last=p;
         p=p->next;
-        last->next=NULL;
     }else{
         third=last=q;
         q=q->next;
-        last->next=NULL;
     }
+    last->next=NULL;
     while(p!=NULL&&q!=NULL){
         if(p->data<q->data){
             last->next=p;
             last=p;
             p=p->next;
-            last->next=NULL;
-
         }else{
             last->next=q;
             last=q;
             q=q->next;
-            last->next=NULL;
-
         }
+        last->next=NULL;
     }
     if(p!=NULL){
         last->next=p;
